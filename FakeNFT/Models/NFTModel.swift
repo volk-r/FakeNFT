73e60f4ac55086581d13eed80f53973d2f528c54
Roman Romanov <rromanov.vk@gmail.com>
2025-02-22 21:27:39 +0500

    [Feature] adding NFTModel

---
+++ FakeNFT/Models/NFTModel.swift
@@ -0,0 +1,18 @@
+//
+//  NFTModel.swift
+//  FakeNFT
+//
+//  Created by Roman Romanov on 22.02.2025.
+//
+
+import Foundation
+
+struct NFTModel: Codable, Equatable {
+    let createdAt: String
+    let name: String
+    let images: [String]
+    let rating: Int
+    let description: String
+    let price: Double
+    let id: String
+}
