2b5f8caff3b5263f200e8799263d5f6767b3f3ef
Sergei Kukharev <kukharev@narod.ru>
2025-02-24 17:48:28 +0300

    [Feature] Added NetworkService service - an alternative to NetworkClient, redesigned for structured multithreading

---
+++ FakeNFT/Services/NetworkService/NetworkService.swift
@@ -0,0 +1,77 @@
+//
+//  NetworkService.swift
+//  FakeNFT
+//
+//  Created by Сергей Кухарев on 24.02.2025.
+//
+
+import Foundation
+
+actor NetworkService: NetworkServiceProtocol {
+    // MARK: - Constants
+
+    private let urlSession: URLSession
+
+    // MARK: - Initializers
+
+    init(urlSession: URLSession = .shared) {
+        self.urlSession = urlSession
+    }
+
+    // MARK: - Public Methods
+
+    func send<T: Sendable & Decodable>(request: NetworkRequest) async throws -> T? where T: Sendable & Decodable {
+        guard let urlRequest = create(request: request) else { return nil }
+
+        do {
+            let (data, response) = try await urlSession.data(for: urlRequest)
+
+            guard let statusCode = (response as? HTTPURLResponse)?.statusCode else {
+                throw NetworkServiceError.invalidResponse
+            }
+
+            guard 200 ..< 300 ~= statusCode else {
+                throw NetworkServiceError.httpStatusCode(statusCode)
+            }
+
+            return try JSONDecoder().decode(T.self, from: data)
+        } catch {
+            if let urlError = error as? URLError {
+                throw NetworkServiceError.urlSessionError(urlError)
+            } else if let decodingError = error as? DecodingError {
+                throw NetworkServiceError.decodingError(decodingError)
+            } else if let networkServiceError = error as? NetworkServiceError {
+                throw networkServiceError
+            } else {
+                throw NetworkServiceError.unknown(error)
+            }
+        }
+    }
+
+    // MARK: - Private Properties
+
+    private func create(request: NetworkRequest) -> URLRequest? {
+        guard let endpoint = request.endpoint else {
+            assertionFailure("Empty endpoint")
+            return nil
+        }
+        var urlRequest = URLRequest(url: endpoint)
+        urlRequest.httpMethod = request.httpMethod.rawValue
+        urlRequest.addValue(GlobalConstants.token, forHTTPHeaderField: "X-Practicum-Mobile-Token")
+
+        if let dtoDictionary = request.dto?.asDictionary() {
+            var urlComponents = URLComponents()
+            let queryItems = dtoDictionary.map { field in
+                URLQueryItem(
+                    name: field.key,
+                    value: field.value
+                    )
+            }
+            urlComponents.queryItems = queryItems
+            urlRequest.httpBody = urlComponents.query?.data(using: .utf8)
+            urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        }
+        urlRequest.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        return urlRequest
+    }
+}
---
+++ FakeNFT/Services/NetworkService/NetworkServiceError.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkServiceError.swift
+//  FakeNFT
+//
+//  Created by Сергей Кухарев on 24.02.2025.
+//
+
+import Foundation
+
+enum NetworkServiceError: Error {
+    case urlRequestError
+    case invalidResponse
+    case httpStatusCode(Int)
+    case urlSessionError(Error)
+    case decodingError(Error)
+    case unknown(Error)
+}
