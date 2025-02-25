db63a82d79af28c552ec3e9016870685c232e911
Sergei Kukharev <kukharev@narod.ru>
2025-02-25 14:52:01 +0300

    [Bug] The principle of naming image resources has been changed

--- FakeNFT/Presentation/AppTabView/AppTabView.swift
+++ FakeNFT/Presentation/AppTabView/AppTabView.swift
@@ -14,19 +14,19 @@ struct AppTabView: View {
         TabView(selection: $selectedTabIndex) {
             ProfileView()
                 .tabItem {
-                    Image(selectedTabIndex == 0 ? "profileActive" : "profileNoActive")
+                    Image(uiImage: selectedTabIndex == 0 ? .profileActive : .profileNoActive)
                     Text("Profile")
                 }
                 .tag(0)
             CatalogView()
                 .tabItem {
-                    Image(selectedTabIndex == 1 ? "catalogActive" : "catalogNoActive")
+                    Image(uiImage: selectedTabIndex == 1 ? .catalogActive : .catalogNoActive)
                     Text("Catalog")
                 }
                 .tag(1)
             CartView()
                 .tabItem {
-                    Image(selectedTabIndex == 2 ? "cartActive" : "cartNoActive")
+                    Image(uiImage: selectedTabIndex == 2 ? .cartActive : .cartNoActive)
                     Text("Cart")
                 }
                 .tag(2)
@@ -34,7 +34,7 @@ struct AppTabView: View {
                 StatisticView()
             }
             .tabItem {
-                Image(selectedTabIndex == 3 ? "statisticActive" : "statisticNoActive")
+                Image(uiImage: selectedTabIndex == 3 ? .statisticActive : .statisticNoActive)
                 Text("Statistic")
             }
             .tag(3)
