825e43f191f79fc765528085117ca71ab29f42b0
Sergei Kukharev <kukharev@narod.ru>
2025-02-26 09:34:40 +0300

    [Bug] Removed NetworkClient inherited from UIKit project template

--- FakeNFT/FakeNFTApp.swift
+++ FakeNFT/FakeNFTApp.swift
@@ -9,15 +9,9 @@ import SwiftUI
 
 @main
 struct FakeNFTApp: App {
-    let servicesAssembly = ServicesAssembly(
-        networkClient: DefaultNetworkClient(),
-        nftStorage: NftStorageImpl()
-    )
-
     var body: some Scene {
         WindowGroup {
             AppTabView()
-                .environment(servicesAssembly)
         }
     }
 }
