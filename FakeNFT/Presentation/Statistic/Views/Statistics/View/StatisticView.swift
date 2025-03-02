895c7fd348de9ae08de618669a9a7f766e911b97
Sergei Kukharev <kukharev@narod.ru>
2025-03-02 15:51:14 +0300

    [Bug] Fix reviewer's comment on moving private enums to extension

--- FakeNFT/Presentation/Statistic/Views/Statistics/View/StatisticView.swift
+++ FakeNFT/Presentation/Statistic/Views/Statistics/View/StatisticView.swift
@@ -8,30 +8,6 @@
 import SwiftUI
 
 struct StatisticView: View {
-    // MARK: - Types
-
-    private enum Constants {
-        static let listAccessibilityIdentifier: String = "statisticsList"
-        static let listEdgeInsets = EdgeInsets(
-            top: 0,
-            leading: 16,
-            bottom: 0,
-            trailing: 16
-        )
-        static let listRowEdgeInsets = EdgeInsets(
-            top: 0,
-            leading: 0,
-            bottom: 8,
-            trailing: 0
-        )
-        static let sortButtonImage: UIImage = .appSortButton
-        static let sortButtonColor: Color = .appBlack
-        static let loadingProgressViewSize: CGFloat = 80
-        static let loadingProgressViewCornerRadius: CGFloat = 12
-        static let loadingDataBackgroundShading: CGFloat = 0.4
-        static let loadingDataBackgroundColor: Color = .appWhite
-    }
-
     // MARK: - State
 
     @State private var viewModel: StatisticViewModelProtocol
@@ -61,63 +37,11 @@ struct StatisticView: View {
                         }
                 }
                 .accessibilityIdentifier(Constants.listAccessibilityIdentifier)
-                .navigationDestination(for: User.self) { user in
-                    UserCardView(user: user, navigationPath: $viewModel.navigationPath)
-                }
-                .navigationDestination(for: UserWebSite.self) { userWebSite in
-                    WebView(navigationURL: userWebSite.website)
-                }
-                .navigationDestination(for: UserId.self) { userId in
-                    Text("\(userId.id)")
-                }
                 .listStyle(.plain)
                 .scrollContentBackground(.hidden)
                 .padding(Constants.listEdgeInsets)
                 .refreshable {
                     await viewModel.reloadData()
-                }
-                .alert(
-                    "",
-                    isPresented: $viewModel.showingErrorAlert,
-                    actions: {
-                        Button("StatisticViewCancelButton", role: .cancel) {}
-                        Button("StatisticViewRetryButton") {
-                            Task {
-                                await viewModel.retryLoadingData()
-                            }
-                        }
-                        .keyboardShortcut(.defaultAction)
-                    },
-                    message: {
-                        Text("StatisticViewErrorText")
-                    }
-                )
-                .confirmationDialog(
-                    "StatisticViewSortingDialogTitle",
-                    isPresented: $viewModel.showingSortDialog,
-                    titleVisibility: .visible
-                ) {
-                    Button("StatisticViewSortingByName") {
-                        viewModel.sortType = .byName
-                    }
-                    Button("StatisticViewSortingByRating") {
-                        viewModel.sortType = .byRating
-                    }
-                    Button("StatisticViewCloseButton", role: .cancel) { }
-                }
-                .toolbar {
-                    ToolbarItem(placement: .primaryAction) {
-                        Button(
-                            action: {
-                                viewModel.sortList()
-                            },
-                            label: {
-                                Image(uiImage: Constants.sortButtonImage)
-                                    .foregroundStyle(Constants.sortButtonColor)
-                            }
-                        )
-                        .disabled(viewModel.loadingState == .loading)
-                    }
                 }
 
                 if viewModel.loadingState == .loading {
@@ -134,11 +58,89 @@ struct StatisticView: View {
                     .allowsHitTesting(true)
                 }
             }
+            .navigationDestination(for: User.self) { user in
+                UserCardView(user: user, navigationPath: $viewModel.navigationPath)
+            }
+            .navigationDestination(for: UserWebSite.self) { userWebSite in
+                WebView(navigationURL: userWebSite.website)
+            }
+            .navigationDestination(for: UserId.self) { userId in
+                Text("\(userId.id)")
+            }
+            .alert(
+                "",
+                isPresented: $viewModel.showingErrorAlert,
+                actions: {
+                    Button("StatisticViewCancelButton", role: .cancel) {}
+                    Button("StatisticViewRetryButton") {
+                        Task {
+                            await viewModel.retryLoadingData()
+                        }
+                    }
+                    .keyboardShortcut(.defaultAction)
+                },
+                message: {
+                    Text("StatisticViewErrorText")
+                }
+            )
+            .confirmationDialog(
+                "StatisticViewSortingDialogTitle",
+                isPresented: $viewModel.showingSortDialog,
+                titleVisibility: .visible
+            ) {
+                Button("StatisticViewSortingByName") {
+                    viewModel.sortType = .byName
+                }
+                Button("StatisticViewSortingByRating") {
+                    viewModel.sortType = .byRating
+                }
+                Button("StatisticViewCloseButton", role: .cancel) { }
+            }
+            .toolbar {
+                ToolbarItem(placement: .primaryAction) {
+                    Button(
+                        action: {
+                            viewModel.sortList()
+                        },
+                        label: {
+                            Image(uiImage: Constants.sortButtonImage)
+                                .foregroundStyle(Constants.sortButtonColor)
+                        }
+                    )
+                    .disabled(viewModel.loadingState == .loading)
+                }
+            }
         }
         .accentColor(.appBlack)
         .task {
             await viewModel.loadData()
         }
+    }
+}
+
+private extension StatisticView {
+    // MARK: - Types
+    
+    private enum Constants {
+        static let listAccessibilityIdentifier: String = "statisticsList"
+        static let listEdgeInsets = EdgeInsets(
+            top: 0,
+            leading: 16,
+            bottom: 0,
+            trailing: 16
+        )
+        static let listRowEdgeInsets = EdgeInsets(
+            top: 0,
+            leading: 0,
+            bottom: 8,
+            trailing: 0
+        )
+        static let sortButtonImage: UIImage = .appSortButton
+        static let sortButtonColor: Color = .appBlack
+        static let loadingProgressViewSize: CGFloat = 80
+        static let loadingProgressViewCornerRadius: CGFloat = 12
+        static let loadingDataBackgroundShading: CGFloat = 0.4
+        static let loadingDataBackgroundColor: Color = .appWhite
     }
 }
 
