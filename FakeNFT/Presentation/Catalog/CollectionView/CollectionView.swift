0ebaf2eea3672354767c03ef918adda718687950
Artem Polivanov <polivanov.a@yandex.ru>
2025-03-02 13:01:10 +0300

    [Feature] Add basic layout of Collection View

--- FakeNFT/Presentation/Catalog/CollectionView/CollectionView.swift
+++ FakeNFT/Presentation/Catalog/CollectionView/CollectionView.swift
@@ -11,35 +11,72 @@ struct CollectionView: View {
     @ObservedObject var viewModel: CollectionViewModel
     @Environment(\.dismiss) private var dismiss
     
+    let columns = [GridItem(.adaptive(minimum: 120), spacing: 16)]
+    
     var body: some View {
-        VStack {
-            Text(viewModel.collection.name.capitalized)
-                .font(.largeTitle)
-                .padding()
+        ScrollView {
+            cover
             
-            if viewModel.nftModels.isEmpty {
-                ProgressView()
-                    .padding()
-            } else {
-                List(viewModel.nftModels) { nft in
-                    Text(nft.name)
+            VStack(alignment: .leading) {
+                title
+                
+                if viewModel.nftModels.isEmpty {
+                    ProgressView()
+                        .padding()
+                } else {
+                    nftCollection
                 }
-                .listStyle(.plain)
             }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .padding(16)
         }
+        .edgesIgnoringSafeArea(.top)
         .navigationBarTitleDisplayMode(.inline)
         .toolbar {
-            ToolbarItem(placement: .navigationBarLeading) {
-                Button {
-                    dismiss()
-                } label: {
-                    Image(systemName: "chevron.left")
-                        .foregroundStyle(.appBlack)
-                }
-            }
+            toolbarItem
         }
         .task {
             await viewModel.loadNFT()
+        }
+    }
+    
+    // MARK: - Subviews
+    
+    private var cover: some View {
+        ImageLoaderFactory(url: viewModel.collection.cover, contentMode: .fill)
+            .frame(maxWidth: .infinity)
+            .frame(height: 310)
+            .cornerRadius(12)
+            .ignoresSafeArea(edges: .top)
+    }
+    
+    private var title: some View {
+        Text(viewModel.collection.name.capitalized)
+            .appTextStyleHeadline3()
+    }
+    
+    private var nftCollection: some View {
+        LazyVGrid(columns: columns, spacing: 16) {
+            ForEach(viewModel.nftModels) { nft in
+                Text(nft.name)
+                    .font(.headline)
+                    .frame(maxWidth: .infinity)
+                    .background(Color.white)
+                    .cornerRadius(12)
+                    .shadow(radius: 2)
+            }
+        }
+        .padding(.top, 16)
+    }
+    
+    private var toolbarItem: some ToolbarContent {
+        ToolbarItem(placement: .navigationBarLeading) {
+            Button {
+                dismiss()
+            } label: {
+                Image(systemName: "chevron.left")
+                    .foregroundStyle(.appBlack)
+            }
         }
     }
 }
