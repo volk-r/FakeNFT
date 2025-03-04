bcaa11fdb6efbe11dd09ff3f31949cebdf757bde
Roman Romanov <rromanov.vk@gmail.com>
2025-03-04 12:29:07 +0500

    [Feature] adding AppStorageKey

---
+++ FakeNFT/Utilites/AppStorageKey.swift
@@ -0,0 +1,15 @@
+//
+//  AppStorageKey.swift
+//  FakeNFT
+//
+//  Created by Roman Romanov on 04.03.2025.
+//
+
+import Foundation
+
+enum AppStorageKey {
+    enum Sorting {
+        static let catalogView = "catalogView"
+        static let myNFTView = "myNFTView"
+    }
+}
