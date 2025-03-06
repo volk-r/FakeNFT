163d5ebdd1f205361864dd95e7524ee32d6f613b
Roman Romanov <rromanov.vk@gmail.com>
2025-03-06 10:31:53 +0500

    [Feature] adding ProfileService

---
+++ FakeNFT/Services/ProfileService/ProfileMockService.swift
@@ -0,0 +1,22 @@
+//
+//  ProfileMockService.swift
+//  FakeNFT
+//
+//  Created by Roman Romanov on 05.03.2025.
+//
+
+import Foundation
+
+actor ProfileMockService: ProfileServiceProtocol {
+    
+    func clearCache() async {
+    }
+    
+    func reloadProfile(for profileId: String) async throws -> ProfileModel? {
+        return try await loadProfile(for: profileId)
+    }
+    
+    func loadProfile(for profileId: String) async throws -> ProfileModel? {
+        return ProfileModel.mockProfile
+    }
+}
