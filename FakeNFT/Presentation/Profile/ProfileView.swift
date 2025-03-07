82503616340ceb8279aac51c6a38f83084a674d1
Artem Polivanov <polivanov.a@yandex.ru>
2025-03-07 21:58:53 +0300

    [Feature] Remove debugging code from Profile View

--- FakeNFT/Presentation/Profile/ProfileView.swift
+++ FakeNFT/Presentation/Profile/ProfileView.swift
@@ -8,39 +8,8 @@
 import SwiftUI
 
 struct ProfileView: View {
-    @EnvironmentObject var profileManager: ProfileManager
-    @State private var nft1: NFTModel = .mock1
-    @State private var nft2: NFTModel = .mock2
-    
     var body: some View {
-        VStack(spacing: 20) {
-            Text("Profile View")
-            
-            NFTCard(
-                imageUrl: nft1.images.first ?? "",
-                isLiked: profileManager.isLiked(nftId: nft1.id),
-                сardType: .normal,
-                isDisabled: profileManager.likeIsDisabled,
-                action: {
-                    Task {
-                        await profileManager.toggleLike(for: nft1.id)
-                    }
-                }
-            )
-            
-            NFTCard(
-                imageUrl: nft2.images.first ?? "",
-                isLiked: profileManager.isLiked(nftId: nft2.id),
-                сardType: .normal,
-                isDisabled: profileManager.likeIsDisabled,
-                action: {
-                    Task {
-                        await profileManager.toggleLike(for: nft2.id)
-                    }
-                }
-            )
-        }
-        .padding()
+        Text("Profile View")
     }
 }
 
