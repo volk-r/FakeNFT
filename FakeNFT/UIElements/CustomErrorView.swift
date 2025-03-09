c7d30f15469c103f546bb07b470a3d7c3443d0fd
Roman Romanov <rromanov.vk@gmail.com>
2025-03-09 11:45:01 +0500

    [Feature] adding CustomErrorView

--- FakeNFT/UIElements/CustomErrorView.swift
+++ FakeNFT/UIElements/CustomErrorView.swift
@@ -5,4 +5,19 @@
 //  Created by Roman Romanov on 09.03.2025.
 //
 
-import Foundation
+import SwiftUI
+
+struct CustomErrorView: View {
+    var body: some View {
+        VStack(alignment: .center, spacing: 10) {
+            Text("Something went wrong")
+                .appTextStyleHeadline3(withColor: .appRedUniversal)
+            Text("Try again later")
+                .appTextStyleHeadline3(withColor: .appRedUniversal)
+        }
+    }
+}
+
+#Preview {
+    CustomErrorView()
+}
