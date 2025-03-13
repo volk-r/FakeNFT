5365275bdb1024e1fd13defbaf1c980dcd074c16
Lavrov Leonid <l.lavrov@kari.com>
2025-03-13 10:11:05 +0500

    [Fix] Fixes after review

--- FakeNFT/Presentation/Cart/Subviews/PurchaseItemView.swift
+++ FakeNFT/Presentation/Cart/Subviews/PurchaseItemView.swift
@@ -43,7 +43,7 @@ struct PurchaseItemView: View {
             Text(item.name)
                 .appTextStyleCaption2()
             Text(item.ticker)
-                .appTextStyleBodyRegular(withColor: .appGreenUniversal)
+                .appTextStyleCaption2(withColor: .appGreenUniversal)
         }
     }
 }
